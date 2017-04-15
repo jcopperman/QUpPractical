5b6bfbb52243a8048bbd1bebef1461cb0b3303f9
Jonathan Opperman <Jonathan Opperman>
2017-04-15 10:40:53 +0200

    Added tests

--- QUpPractical.Tests/UiTests/CreateCustomerTests.cs
+++ QUpPractical.Tests/UiTests/CreateCustomerTests.cs
@@ -3,10 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TechTalk.SpecFlow;
 
 namespace QUpPractical.Tests.UiTests
 {
-    class CreateCustomerTests
+    public class CreateCustomerTests
     {
+        [Given(@"I have navigated to the website")]
+        public void GivenIHaveNavigatedToTheWebsite()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Given(@"I have selected Customers")]
+        public void GivenIHaveSelectedCustomers()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Given(@"I have clicked Create")]
+        public void GivenIHaveClickedCreate()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Given(@"I have entered valid information")]
+        public void GivenIHaveEnteredValidInformation()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"I click Save")]
+        public void WhenIClickSave()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"the customer record is saved successfully")]
+        public void ThenTheCustomerRecordIsSavedSuccessfully()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
     }
 }
